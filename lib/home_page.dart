70b5a55dd1903db9622103cc54c8dee674984942
Anand Shanker <anand.shanker@inditrade.com>
2022-06-03 16:55:40 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+ class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  var days=30;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First APP"),
+      ),
+        body: Center(
+          child: Container(
+            child:Text("Welocome to $days",
+            style: TextStyle(color: Colors.red),),
+           ),
+        ),
+        drawer: Drawer(),
+      );
+    
+  }
+}
